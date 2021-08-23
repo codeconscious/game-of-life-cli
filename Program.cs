f64164ad52b95192ba9883943d7b0f07ab076324
CodeConscious <hello@codeconscio.us>
2021-08-23 10:09:21 +0900

    Add game time to the final output; change "--debug" to "--default"

--- Program.cs
+++ Program.cs
@@ -7,16 +7,18 @@
             "   - Number of rows .......... 3 to 256 (inclusive) or else -1 to fit the console height\n" +
             "   - Number of columns ....... 3 to 256 (inclusive) or else -1 to fit the console width\n" +
             "   - Initial density (%) ..... 1 to 99 (inclusive, digits only) or else -1 to set randomly\n" +
-            "   - (Optional) Delay between iterations in milliseconds";
+            "   - (Optional) Delay between iterations in milliseconds (Default is 50)\n" +
+            "Alternatively, supply only \"--default\" to use the default settings.";
 
         private static void Main(string[] args)
         {
-            // Prepare the grid settings.
+            // Prepare the settings from the given args.
             Settings settings;
-            if (args.Length == 1 && args[0] == "--debug")
+
+            if (args.Length == 1 && args[0] == "--default")
             {
-                WriteLine("Proceeding with default settings.");
-                settings = new Settings(new[] { "35", "150", "40" });
+                WriteLine("Using default settings.");
+                settings = new Settings(new[] { "-1", "-1", "-1" });
             }
             else
             {
@@ -29,6 +31,7 @@
                     ForegroundColor = ConsoleColor.Yellow;
                     WriteLine(ex.Message);
                     ResetColor();
+
                     WriteLine(Instructions);
                     return;
                 }
@@ -53,31 +56,33 @@
         }
 
         /// <summary>
-        /// Generate the grid and run endlessly or until the grid is dead or stale.
+        /// Generate the grid and run endlessly or until the grid enters a non-alive state.
         /// </summary>
         /// <param name="settings"></param>
         private static void RunGame(Settings settings)
         {
             nuint iteration = 1;
 
-            var startTime = DateTime.Now;
+            var gameStartTime = DateTime.Now;
+            var iterationStartTime = gameStartTime;
 
             Grid grid = new(settings);
             grid.Print();
 
-            var duration = DateTime.Now - startTime;
+            var duration = DateTime.Now - iterationStartTime;
             var outputRow = grid.RowCount + 1;
 
-            PrintStatus(iteration, duration, outputRow);
+            PrintIterationSummary(iteration, duration, outputRow);
 
             // Process and print grid updates
             do
             {
+                // Stop if the user pressed a key.
                 if (Console.KeyAvailable)
                     break;
 
                 iteration++;
-                startTime = DateTime.Now;
+                iterationStartTime = DateTime.Now;
 
                 var cellsToUpdate = grid.GetUpdatesForNextIteration();
 
@@ -85,23 +90,26 @@
 
                 grid.PrintUpdates(cellsToUpdate, settings.IterationDelay);
 
-                duration = DateTime.Now - startTime;
+                duration = DateTime.Now - iterationStartTime;
 
-                PrintStatus(iteration, duration, outputRow);
+                PrintIterationSummary(iteration, duration, outputRow);
             }
             while (grid.Status == GridStatus.Alive);
 
-            PrintResults(grid.Status, iteration, outputRow);
+            var gameDuration = DateTime.Now - gameStartTime;
+
+            PrintGameResults(grid.Status, iteration, gameDuration, outputRow);
         }
 
-        private static void PrintStatus(nuint iteration, TimeSpan duration, int outputRow)
+        private static void PrintIterationSummary(nuint iteration, TimeSpan duration, int outputRow)
         {
             SetCursorPosition(0, outputRow);
             WriteLine($"Iteration {iteration:#,##0} ({duration.TotalMilliseconds:#,##0}ms)  ");
-            Write("Press any key to quit."); // TODO: Move elsewhere
+            Write("Press any key to quit.");
         }
 
-        private static void PrintResults(GridStatus finalStatus, nuint iterations, int outputRow)
+        private static void PrintGameResults(GridStatus finalStatus, nuint iterations,
+                                             TimeSpan duration, int outputRow)
         {
             var statusStatement = finalStatus switch
             {
@@ -111,9 +119,9 @@
                 _ => "An unexpected state was reached"
             };
 
-            SetCursorPosition(0, outputRow);
+            SetCursorPosition(0, outputRow + 1);
 
-            WriteLine($"{statusStatement} after {iterations:#,##0} iterations.");
+            Write($"{statusStatement} after {iterations:#,##0} iterations in {duration.TotalSeconds:#,##0.##}s.");
         }
     }
 }
