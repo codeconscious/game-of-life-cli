6ff1697e4bf556bdfa17e40a4ad749a48b15f473
CodeConscious <hello@codeconscio.us>
2021-08-28 19:57:57 +0900

    Add System.Threading.Tasks

--- Usings.cs
+++ Usings.cs
@@ -3,5 +3,6 @@ global using System.Collections.Generic;
 global using System.Collections.Concurrent;
 global using System.Linq;
 global using System.Threading;
+global using System.Threading.Tasks;
 global using System.Diagnostics;
 global using static System.Console;
