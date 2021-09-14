544f5d98dad58e06200fe477e57cf6d2bd206a57
CodeConscious <hello@codeconscio.us>
2021-09-14 21:01:10 +0900

    Add comments

--- GridState.cs
+++ GridState.cs
@@ -1,11 +1,33 @@
 namespace GameOfLife
 {
+    /// <summary>
+    /// An enum indicating the state of the grid/game.
+    /// </summary>
     public enum GridState
     {
+        /// <summary>
+        /// The initial state indicating the game is ongoing.
+        /// </summary>
         Alive,
-        Looping, // Stuck in an infinite loop.
-        Stagnated, // No further changes in future iterations.
-        Extinct, // No living cells remaining.
+
+        /// <summary>
+        /// The grid is trapped in an infinite loop.
+        /// </summary>
+        Looping,
+
+        /// <summary>
+        /// There can be no further changes in any future iterations.
+        /// </summary>
+        Stagnated,
+
+        /// <summary>
+        /// No living cells remain.
+        /// </summary>
+        Extinct,
+
+        /// <summary>
+        /// The user ended the game.
+        /// </summary>
         Aborted
     }
 }
