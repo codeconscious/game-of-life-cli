a5c935289cf1dc2c690227685fc0755ffe64b373
CodeConscious <hello@codeconscio.us>
2021-09-18 22:07:31 +0900

    Add first tests; reorganize file locations

---
+++ src/GameOfLife.Tests/SettingsTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using GameOfLife;
+
+namespace GameOfLife.Tests;
+
+public class SettingsTests
+{
+    [Fact]
+    public void Constructor_Succeeds_With3ProperArguments()
+    {
+        var args = new string[] { "-1", "-1", "20" };
+        var settings = new GameOfLife.Settings(args);
+
+        Assert.NotNull(settings);
+        Assert.Equal(20, settings.InitialPopulationRatio);
+    }
+
+    [Fact]
+    public void Constructor_ThrowsArgumentException_WhenTooManyArguments()
+    {
+        var args = new string[] { "-1", "-1", "20", "20", "100" };
+        //var settings = new GameOfLife.Settings(args);
+
+        Assert.Throws<ArgumentException>(() => new GameOfLife.Settings(args));
+    }
+}
