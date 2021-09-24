4f6563cef039d0dd1a61a6b69a8924a5a395cfd8
CodeConscious <hello@codeconscio.us>
2021-09-24 19:33:53 +0900

    Create GridTests.cs

---
+++ src/GameOfLife.Tests/GridTests.cs
@@ -0,0 +1,22 @@
+using Xunit;
+using GameOfLife;
+
+namespace GameOfLife.Tests
+{
+    [Theory]
+    [InlineData("300", "50", "90")]
+    [InlineData("50", "30", "20")]
+    [InlineData("3", "3", "1")]
+    [InlineData("20000", "20000", "99")]
+    public void Constructor_Succeeds_WithValidSettings
+        (string width, string height, string populationRatio, string delay = "50")
+    {
+        var args = new string[] { width, height, populationRatio, delay };
+        var settings = new Settings(args);
+
+        Assert.NotNull(settings);
+        Assert.Equal(ushort.Parse(width), settings.Width);
+        Assert.Equal(ushort.Parse(height), settings.Height);
+        Assert.Equal(byte.Parse(populationRatio), settings.InitialPopulationRatio);
+    }
+}
