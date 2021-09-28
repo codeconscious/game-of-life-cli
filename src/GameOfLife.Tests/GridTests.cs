da65d645c9bca39d01393d9eac1eb4d905fdad45
CodeConscious <hello@codeconscio.us>
2021-09-28 12:28:45 +0900

    Decrease size of test grid to increase test speed

--- src/GameOfLife.Tests/GridTests.cs
+++ src/GameOfLife.Tests/GridTests.cs
@@ -9,7 +9,7 @@ public class GridTests
     [InlineData("300", "50", "90")]
     [InlineData("50", "30", "20")]
     [InlineData("3", "3", "1")]
-    [InlineData("20000", "20000", "99")]
+    [InlineData("2000", "2000", "99")]
     public void Constructor_Succeeds_WithValidSettings
         (string width, string height, string populationRatio, string delay = "50")
     {
