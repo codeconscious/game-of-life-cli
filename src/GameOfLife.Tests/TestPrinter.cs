72e009d2757a6950868e2dda61b1f97b27e919b4
CodeConscious <hello@codeconscio.us>
2021-09-30 22:38:13 +0900

    Remove empty lines

--- src/GameOfLife.Tests/TestPrinter.cs
+++ src/GameOfLife.Tests/TestPrinter.cs
@@ -4,33 +4,15 @@ namespace GameOfLife.Tests;
 
 public class TestPrinter : IPrinter
 {
-    public void Print(string text)
-    {
+    public void Print(string text) { }
 
-    }
+    public void PrintEntire(Grid grid, bool shouldClear) { }
 
-    public void PrintEntire(Grid grid, bool shouldClear)
-    {
+    public void PrintGameSummary(Grid grid) { }
 
-    }
+    public void PrintIterationSummary(Grid grid, TimeSpan? duration = null) { }
 
-    public void PrintGameSummary(Grid grid)
-    {
+    public void PrintLine(string text) { }
 
-    }
-
-    public void PrintIterationSummary(Grid grid, TimeSpan? duration = null)
-    {
-
-    }
-
-    public void PrintLine(string text)
-    {
-
-    }
-
-    public void PrintUpdates(Grid grid, List<Cell> cellsForUpdate)
-    {
-
-    }
+    public void PrintUpdates(Grid grid, List<Cell> cellsForUpdate) { }
 }
